cd7ac74defd09c81a23d1886550aa05a355b107b
Deepu714 <deepika.d@worthitcs.com>
2024-08-12 13:05:43 +0530

    toastify

--- src/login.js
+++ src/login.js
@@ -1,13 +1,17 @@
 import {useState} from 'react';
+import {Bounce,ToastContainer,toast} from 'react-toastify';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {FaEye,FaEyeSlash} from 'react-icons/fa';
+
+import 'react-toastify/dist/ReactToastify.css';
 
    const Login=()=>{
     // const myFunction=()=>{(setValue("Submit"))}
         const[entries,setEntries]=useState([])
        const[table,setTable]=useState(false)
        const[showModal,setshowModal]=useState(false)
-       const[showPassword,setShowPassword]=useState(false)      
+       const[showPassword,setShowPassword]=useState(false)  
+    //    const[msgdisplay,setmsgdisplay]=useState(false)
     const myFunction=()=>{
     setTable(true);
     if(Uname.trim() !==""&&password.trim()!==""){
@@ -32,9 +36,25 @@ const toggleModal=()=>{
 const toggleShowPassword=()=>{
     setShowPassword(!showPassword);
 }
+
+const msgdisplay=()=>{
+    toast.success('Submitted', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        });
+}
+
    return(
 
     <div className="row justify-content-center">
+    
    <div className='col-md-3 mt-6'>
    <h1>Login</h1>
 
@@ -43,7 +63,6 @@ const toggleShowPassword=()=>{
     <label for="password">Password :</label><br/>
     <div className='input-group'><input type={showPassword? "text":"password"} id="password" name="Password" onChange={(e)=>handlepassword(e)} />
     <button type='button' className='btn btn-outline-secondary' onClick={toggleShowPassword}>{showPassword? <FaEyeSlash/>:<FaEye/>}</button>
-
     </div>
     <br/><br/>
     <button onClick={myFunction} className="btn btn-primary">Login</button><br/><br/>
@@ -53,22 +72,25 @@ const toggleShowPassword=()=>{
         <div className='modal-dialog'>
             <div className='modal-content'>
                 <div className='modal-header'>
-    <h5 className='modal-title'>SIGNEDIN</h5>
-    <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='close'></button> 
+    <h5 className='modal-title'>SIGNUP</h5>
+    {/* <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='close'></button>  */}
+    <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='close' onClick={toggleModal}></button>
     </div>
     <div className='modal-body'>
-        <p>USERNAME :</p>
-        <p>EMAIL :</p>
-        <p>PASSWORD :</p>
+        <label for="Email">E-MAIL:</label><br/>
+            <input type="text" id="Email" name="Email"/><br/>
+            <label for ="PASSWORD">PASSWORD:</label><br/>
+            <input type="text" id="PASSWORD" name="PASSWORD"/>
     </div>
     <div className='modal-footer'>
         <button type='button' className='btn btn-secondary' data-bs-dismiss='close' onClick={toggleModal}>CLOSE</button>
-        <button type='button' className='btn btn-primary'>SUBMIT</button>
+        <button type='button' className='btn btn-primary' onClick={msgdisplay}>SUBMIT</button>
     </div>
     </div>
     </div>
     </div>
     )}
+    
     {table &&(
     <div className="table-responsive">
     <table className="table table-bordered table-primary">
@@ -93,6 +115,21 @@ const toggleShowPassword=()=>{
 }
    
    </div>
+   <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+transition= {Bounce}
+/>
+{/* Same as */}
+<ToastContainer />
    </div>
 
 
